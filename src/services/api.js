23e3419efe75b8f1aef402b7f36ce0795465e418
Eduardo Martínez Varinas <yayomar211@gmail.com>
2025-08-27 09:37:09 -0600

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL || '/api', 
 });
+console.log("VITE_API_URL: ", import.meta.env.VITE_API_URL);
 
 export default API;
